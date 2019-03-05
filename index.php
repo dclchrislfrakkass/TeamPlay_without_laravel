78c64013cc57b049d819022073ab87e6d37eaaa2
ChrisL_frakkass <dcl.chrisl@18pixel.fr>
2019-03-06 00:58:22 +0100

    work on connect but don't work bindparam??

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php $title = 'Accueil'; ?>
 
 <?php ob_start(); ?>
@@ -53,13 +54,14 @@
                             <label>Mot de passe</label>  
                             <input type="password" name="password" id="password" class="form-control" required/>
                             <br/>
-                            <label>Confirmation du mot de passe</label>  
+                            <!-- <label>Confirmation du mot de passe</label>  
                             <input type="password" name="repeatPassword" id="repeatPassword" class="form-control" required/>
-                            <br/>
+                            <br/> -->
                             <button type="submit" name="login_button" id="login_button" class="btn btn-warning">Connexion</button>
                         </form>
                     </div>  
             </div>  
+
         </div>  
     </div>  
     <div id="registerModal" class="modal fade" role="dialog">  
@@ -71,7 +73,7 @@
                         <button type="button" class="close" data-dismiss="modal"><i class="fas fa-times"></i></button>  
                     </div>  
                     <div class="modal-body">  
-                        <form method="post" action="">
+                        <form method="post" action="register.php">
                             <label>Pseudo</label>  
                             <input type="text" name="username" id="username" class="form-control" required/>  
                             <br/>  
@@ -88,72 +90,10 @@
                         </form>
                     </div>  
             </div>  
+            
         </div>  
 
-        <?php require_once 'work/pdo.php'; 
-
-        $errors = array();
-
-
-        if (isset($_POST['register_button']))
-        {
-
-        /* on test si les champ sont bien remplis */
-            if(!empty($_POST['username']) and !empty($_POST['email']) and !empty($_POST['password']) and !empty($_POST['repeatPassword']))
-            {
-
-                $username = htmlspecialchars(trim($_POST['username']));
-                $email = htmlspecialchars(trim($_POST['email']));
-                $password = htmlspecialchars(trim($_POST['password']));
-                $repeatPassword = htmlspecialchars(trim($_POST['repeatPassword']));
-
-
-                /* on test si le mdp contient bien au moins 6 caractère */
-                if (strlen($_POST['password'])>=6)
-                {
-                    /* On test si le MDP est rentré, et si les deux MDP ne sont pas différent */
-                    if (empty($_POST['password']) || $_POST['password'] != $_POST['repeatPassword'])
-                    {
-                        $errors['pass'] = "Vous devez rentrer un mot de passe valide";
-
-                    } else {
-                        // Vérifie si l'utilisateur n'est pas déjà enregistré
-                        $req = $bd->prepare("SELECT name FROM users WHERE name = :username");
-                        $req->bindParam(':username', $_POST['username']);
-                        $membre = $req->fetch();
-
-                        if ($membre) {
-                            
-                            echo '<div class="mx-auto mt-5>';
-                            echo 'Ce nom est déjà utilisé !';
-                            echo '</div>';
-                    
-                        } else {
-                            // Insertion dans la base de donnée
-                            $creationUtilisateur = $bd->prepare("INSERT INTO users SET name = :username, email = :email, password = :password, idRole = '2'");
-                            $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-                            $creationUtilisateur->bindParam(':username', $_POST['username']);
-                            $creationUtilisateur->bindParam(':email', $_POST['email']);
-                            $creationUtilisateur->bindParam(':password', $password);
-                            $creationUtilisateur->execute();
-                            
-                            echo "<script>alert(\"Utilisateur ".$username. " enregistré\")</script>";
-                            // echo '<br/>';
-                            // echo '<div class="mx-auto mt-5>';
-                            // echo $username. " a bien été enregistré !<br/>";
-                            // echo '</div>';
-                        }
-
-                    }
-            
-                }
-                else echo "<script>alert(\"Le mot de passe est trop court !\")</script>";
-            }
-            else echo "<script>alert(\"Veuillez saisir tous les champs !\")</script>";
-        }
-
-        ?>
-        
+       
     </div>  
   
 
