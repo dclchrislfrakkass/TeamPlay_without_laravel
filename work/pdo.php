3d759fe0c6aeb361e606672203a58f960b06528c
DclFabienLycan <dcl.fabienb@18pixel.fr>
2019-03-05 14:34:05 +0100

    ajout login + signin

--- work/pdo.php
+++ work/pdo.php
@@ -1,10 +1,12 @@
 <?php
 
 try {
-$json =file_get_contents('pdo.JSON');
+$json =file_get_contents('work/pdo.JSON');
 $dec=json_decode($json, true);
 
 $bd=new PDO("mysql:host=".$dec['host'].";dbname=".$dec['dbName'], $dec['user'] , $dec['pass']);
+$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$bd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 //stock url 51.254.203.143
 $bd->exec('SET NAMES utf8');
     
