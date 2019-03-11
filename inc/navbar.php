b009da3c299741ec833d9105f0883e23e3c55210
ChrisL_frakkass <dcl.chrisl@18pixel.fr>
2019-03-11 14:30:31 +0100

    delete some var_dumps and push for lycan

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,10 +1,10 @@
 <?php
 session_start(); 
 if (isset($_SESSION)){
-    echo '<br/><br/>';
-    print_r['auth'];
-    $user = $_SESSION['auth']->name;}
-
+    // echo '<br/><br/>';
+    // print_r['auth'];
+    // $user = $_SESSION['auth']->name;}
+}
 // $_SESSION['name'] = $pseudoMembre;
 ?>
 
